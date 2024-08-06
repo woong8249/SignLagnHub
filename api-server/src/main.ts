cc06c6178c24c5c788e1c741f26f3c55e01d08a9
woong8249 <woong8249@gmail.com>
2024-08-06 22:58:56 +0900

    Fix nest system logger to MyLogger

--- api-server/src/main.ts
+++ api-server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import fs from 'fs';
+import { MyLogger } from './loggers/logger.service';
 
 async function bootstrap() {
   const uploadDir = join(__dirname, '..', 'uploads');
@@ -9,6 +10,7 @@ async function bootstrap() {
     fs.mkdirSync(uploadDir);
   }
   const app = await NestFactory.create(AppModule);
+  app.useLogger(await app.resolve(MyLogger));
   await app.listen(3000);
 }
 bootstrap();
